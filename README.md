cda2b5151555b318ff28da540a4db50cae8baca6
DeepakPatil26 <deepakpatil2612@gmail.com>
2023-06-13 17:28:39 +0530

    Add Bootstrap UI Framework

--- README.md
+++ README.md
@@ -15,3 +15,29 @@
    1. npm i react-router-dom
    2. create route for home screen
    3. create route for product screen
+7. Create Node.JS-Server
+   1. run npm init in root folder
+   2. Update package.json set type:module
+   3. Add .js to imports
+   4. npm install express
+   5. create server.js
+   6. add start command as node backend/server.js
+   7. require express
+   8. create route for / return backend is ready.
+   9. move product.js from frontend to backend.
+   10. create route for /api/products
+   11. return products
+   12. run npm start
+8. Fetch products from backend
+   1. set proxy in package.json
+   2. npm install axios
+   3. use state hook
+   4. use effect hook
+   5. use reducer hook
+9. Manage State by Reducer hook
+   1. define reducer
+   2. update fetch data
+   3. get state from useReducer
+10. Add Bootstrap UI Framework
+    1. npm install react-bootstrap bootstrap
+    2. update App.js
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,71 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useReducer } from 'react';
+// import data from '../data';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, products: action.payload };
+    case 'FETCH_FAILURE':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 const HomeScreen = () => {
+  // const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    error: '',
+    products: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAILURE', payload: error.message });
+      }
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className='products'>
-        {data.products.map((product) => (
-          <div key={product.slug} className='product'>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className='product-info'>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div key={product.slug} className='product'>
               <Link to={`/product/${product.slug}`}>
-                {' '}
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>$ {product.price}</strong>
-              </p>
-              <button>Add to Cart</button>
+              <div className='product-info'>
+                <Link to={`/product/${product.slug}`}>
+                  {' '}
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>$ {product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
