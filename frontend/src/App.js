06ce77744acb95271bb085170b1e595c6634f972
DeepakPatil26 <deepakpatil2612@gmail.com>
2023-06-13 11:25:38 +0530

    List Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
+/* eslint-disable array-callback-return */
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +8,28 @@ function App() {
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  {' '}
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>$ {product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
