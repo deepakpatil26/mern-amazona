14ba5b28deb001ef4541b3bdc1607a84464bca2d
DeepakPatil26 <deepakpatil2612@gmail.com>
2023-06-15 11:25:51 +0530

    Complete Cart Screen

--- README.md
+++ README.md
@@ -61,3 +61,11 @@
 15. Complete Add to Cart
     1. check exist item in the cart
     2. check count in stock in backend
+16. Create Cart Screen
+    1. create 2 columns
+    2. display items list
+    3. create action column
+17. Complete Cart Screen
+    1. click handler for inc/dec item
+    2. click handler for remove item
+    3. click handler for checkout
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,11 +1,32 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useContext } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Rating from './Rating';
+import axios from 'axios';
+import { Store } from '../Store';
 
 const Product = (props) => {
   const { product } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -21,7 +42,13 @@ const Product = (props) => {
           numReviews={product.numReviews}
         ></Rating>
         <Card.Text>$ {product.price}</Card.Text>
-        <Button>Add to Cart</Button>
+        {product.countInStock === 0 ? (
+          <Button variant='light' disabled>
+            Out of Stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add to Cart</Button>
+        )}
       </Card.Body>
     </Card>
   );
